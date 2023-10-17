43f533362d8655f4026d3b6a5c5c9e8b3bb56b41
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-17 20:05:09 +0500

    Update sirREcCode.cpp

--- day 4 code/sirREcCode.cpp
+++ day 4 code/sirREcCode.cpp
@@ -7,8 +7,10 @@ int main(){
      // int b = 15;
     int a=14, b=15;
     float pi=3.15;
+    char c ='d';
     
     cout<<"This is tutorial 4. here the value of a is " <<a<<".  The value of b is " << b<<endl; //'\n' is same as endl
     cout<<"The value of pi is: "<<pi;
+    cout<<"\nThe value of c is: "<<c;
     return 0;
 }
