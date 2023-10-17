8ad2e2583adab9ef6c4f372b1ee2f62e79abe593
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-17 19:47:26 +0500

    Create sir.cpp
    This is cpp learning code

---
+++ day 4 code/sir.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=14;
+    int b=15;
+    cout<<"This is tutorial 4. here the value of a is " <<a<<".  The value of b is" << b;
+
+    return 0;
+}
