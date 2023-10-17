e042b6a9eef60b48f26e9a402e44626bcd55417d
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-17 19:53:44 +0500

    Create sirREcCode.cpp
    Same as sir.cpp

---
+++ day 4 code/sirREcCode.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){       
+     // int a = 14;
+     // int b = 15;
+    int a=14, b=15;
+  
+    cout<<"This is tutorial 4. here the value of a is " <<a<<".  The value of b is " << b;
+
+    return 0;
+}
