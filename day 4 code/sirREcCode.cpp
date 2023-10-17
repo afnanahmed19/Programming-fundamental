4be3e63edb48a64ddc73e9f81d4d1abfed0072f3
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-17 19:58:21 +0500

    Update sirREcCode.cpp

--- day 4 code/sirREcCode.cpp
+++ day 4 code/sirREcCode.cpp
@@ -6,8 +6,9 @@ int main(){
      // int a = 14;
      // int b = 15;
     int a=14, b=15;
-  
-    cout<<"This is tutorial 4. here the value of a is " <<a<<".  The value of b is " << b;
-
+    float pi=3.15;
+    
+    cout<<"This is tutorial 4. here the value of a is " <<a<<".  The value of b is " << b<<endl; //'\n' is same as endl
+    cout<<"The value of pi is: "<<pi;
     return 0;
 }
