39769d4f097d22b4f2bb4e0fbc69f92bf0082c72
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-19 19:01:40 +0500

    Create harrysirVid01.cpp
    Topic coment //  /*__*/

---
+++ day 1 code/harrysirVid01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ using namespace std;
+
+ int main(){
+    //this is comment
+    /* 
+        This is multi comment
+    */
+   int sum=5;
+    cout<<"Hello World MY NAME IS AFNAN " <<  sum <<'\n'; // << this is insertion operator
+    cout<<"i like pizza" <<'\n';
+    return 0;
+ }
