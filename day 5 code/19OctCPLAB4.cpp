4cfc7e62c9945fed9f0c8dc1495ffac116df3438
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-19 10:29:03 +0500

    Create 19OctCPLAB4.cpp
    this lab task 04 cp

---
+++ day 5 code/19OctCPLAB4.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+/*Question: Declare an integer variable age and a double variable height. Initialize age with 25 and height
+with 5.9. Print both variables to the console. */
+
+int main(){
+
+
+int age = 25;
+double height = 5.9;
+
+
+cout<< "user Age: "<< age<<endl;
+cout<< "user height: "<< height <<endl;
+
+
+
+	
+}
+/* #include<iostream>
+using namespace std;
+/*Write a C++ program that takes the user&#39;s name as input and then prints a greeting message
+using that name. For example, if the user enters &quot;John,&quot; the program should print &quot;Hello, John!&quot;. */
+
+int main(){
+
+string name;
+
+cout<< "please enter your name: ";
+cin>> name;
+
+cout <<"Hello,"<< name<<"!"<<endl;
+return 0;
+
+
+	
+}*/
+
+/*#include<iostream>
+using namespace std;
+/*Question: Write a C++ program to calculate the area of a rectangle. Prompt the user to enter the length
+and width of the rectangle. Use appropriate data types for length and width, and output the calculated
+area.
+ */
+
+int main(){
+//a=l*b
+double length; //variable for decimal value
+double breadth;
+ 
+cout<<"enter the length ";
+cin>>length;
+
+cout<<"enter the braedth";
+cin>>breadth;
+
+double Area=length*breadth;
+
+cout<<"Area of rectangle is: "<< Area <<endl;
+return 0;
+	
+}*/
+
+
+
+  
+/*
+#include<iostream>
+using namespace std;
+/*Create a C++ program that calculates the circumference of a circle. Declare a constant
+variable pi with a value of 3.14159 and a double variable radius. Prompt the user to enter the radius of
+the circle and calculate the circumference using the formula 2 * pi * radius..
+ */
+
+int main(){
+
+float pi=3.14159;
+
+double radius;
+
+cout<<"plz enter circle radius in meter: ";
+cin>>radius;
+
+double circumference=2*pi*radius;
+
+cout<<"circumference of a circle"<<" "<< circumference<<"m";
+
+return 0;
+}
+	
+
+*/
