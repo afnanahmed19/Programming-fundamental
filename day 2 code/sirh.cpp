4dcb00a0893a111fdac06518099ceb9a32975921
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-20 23:28:01 +0500

    Create sirh.cpp
    uses of const variable

---
+++ day 2 code/sirh.cpp
@@ -0,0 +1,10 @@
+#include<iostrem>
+using namespace std;
+
+int main() {
+      // The const keyword specifies that avariable's value is constant    
+  // tells the compiler to prevant anything from modifying it
+    // (read- only)
+const double Height=1000;
+  const double Pi=3.141;
+} 
