ea332b3a31468d0ca60d41a4483308a6591becaf
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-23 22:16:24 +0500

    Create basicvariable.cpp
    basic variable concepts baby don't hurt me

---
+++ day 3 code/basicvariable.cpp
@@ -0,0 +1,37 @@
+# include <iostream>
+ using namespace std;
+  int main() { 
+    int x=5;
+    int y=6;
+    int sum= x + y;
+
+        // single character 
+        char grade = 'A';
+        char initial = 'C';
+        char currency = '$';
+        
+      //integer (whole number)
+      int age = 20;
+      int year = 2023;
+      int days = 7;
+
+      // boolean (true or false)
+      bool student =false;
+      bool power = true;
+      bool forSale = true;
+
+      //double (number including decimal
+       double price = 10.99;
+       double gpa = 2.5;
+       double temperature = 25.1;
+
+      // string (objects that represents a sequence of text)
+    
+      string name="Afnan";
+      string day = "Friday";
+      string address = "123 fake st.";
+
+      cout << "hello " << name << '\n' ;
+      cout<< "you are " << age << " year old";
+    return 0;
+  }
