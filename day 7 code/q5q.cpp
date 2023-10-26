4652c1e61b3e911104059fbc3dc1c227f4dcb875
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-26 10:39:41 +0500

    Update q5q.cpp

--- day 7 code/q5q.cpp
+++ day 7 code/q5q.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     int totalClasses, attendedClasses;
 
     // Ask the user for the number of classes held and attended
-    std::cout << "Please enter the total number of classes held: ";
-    std::cin >> totalClasses;
+    cout << "Please enter the total number of classes held: ";
+    cin >> totalClasses;
 
-    std::cout << "Please enter the number of classes you've attended: ";
-    std::cin >> attendedClasses;
+    cout << "Please enter the number of classes you've attended: ";
+    scin >> attendedClasses;
 
     // Calculate attendance percentage
     double attendancePercentage = (static_cast<double>(attendedClasses) / totalClasses) * 100;
 
     // Check if the attendance meets the requirements
     if (attendancePercentage >= 75.0) {
-        std::cout << "Your attendance percentage is " << attendancePercentage << "%, and you are allowed to sit in the exam." << std::endl;
+        cout << "Your attendance percentage is " << attendancePercentage << "%, and you are allowed to sit in the exam." << endl;
     } else {
-        std::cout << "Your attendance percentage is " << attendancePercentage << "%, and you do not meet the requirements to sit in the exam." << std::endl;
+        cout << "Your attendance percentage is " << attendancePercentage << "%, and you do not meet the requirements to sit in the exam." << endl;
     }
 
     return 0;
