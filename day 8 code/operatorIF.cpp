66a1d279c90f186bf06c87c5a8abba51005634fd
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-10-31 09:23:59 +0500

    Create operatorIF.cpp
    uses of IF operator

---
+++ day 8 code/operatorIF.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    if (a > b) {
+        std::cout << "a is greater than b" << std::endl;
+    } else {
+        std::cout << "a is not greater than b" << std::endl;
+    }
+
+    return 0;
+}
