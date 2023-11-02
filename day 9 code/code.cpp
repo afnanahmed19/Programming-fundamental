47e03d9e398c148c31dac1bc7d149c9ae7fd8aeb
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-11-02 09:35:06 +0500

    Update

---
+++ day 9 code/code.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2, result;
+
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << num1 << " + " << num2 << " = " << result;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << num1 << " - " << num2 << " = " << result;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << num1 << " * " << num2 << " = " << result;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << num1 << " / " << num2 << " = " << result;
+            } else {
+                cout << "Error: Division by zero is not allowed.";
+            }
+            break;
+        default:
+            cout << "Error: Invalid operator";
+            break;
+    }
+
+    return 0;
+}
