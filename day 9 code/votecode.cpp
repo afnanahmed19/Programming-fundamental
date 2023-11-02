4856d9e261ff0cc3af7e280a3cab9775917ebc80
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-11-02 13:03:33 +0000

    hehe boy

--- day 9 code/votecode.cpp
+++ day 9 code/votecode.cpp
@@ -8,13 +8,13 @@ int main(){
 	cout<<"tell me your age: "<<endl;
 	cin>>age;
 	
-	if((age<18)&&(age>0)) {
+	if((age<18)&&(age<0)) {
 		cout<<"you are not eligible for voting"<<endl;
 	}
 	else if (age==18){
 	cout<<"you are eligible for voting"<<endl; }
 	else if (age<1){
-		cout<<"your are not yet born"<<endl;
+		cout<<"you are not yet born"<<endl;
 	}
 	else {
 		cout<<"you are welcome for voting :)"<<endl;
