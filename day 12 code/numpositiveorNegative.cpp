12da003f8ba924308b46a74c362f206570d6cd0c
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-11-19 11:38:38 +0000

    condition

---
+++ day 12 code/numpositiveorNegative.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    
+    cout<<" plz enter the value of n :"<<endl;
+     cin>>a;
+     
+    //if a is positive
+    if(a>0){
+        cout<<"n valve is positive"<<endl;
+    }
+    else{
+        cout<<"n value is negative"<<endl;
+    }
+    return 0;
+}
