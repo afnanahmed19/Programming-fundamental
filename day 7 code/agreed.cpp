74671fee7f17e92406cb81a1cf42e62cf2b1cd2b
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-12-03 20:06:03 +0000

    Hehe

---
+++ day 7 code/agreed.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  
+
+    char c;
+    cout<<"do you agree";
+    cin>>c;
+
+    if (c=='y'){
+        cout<<"your agreed";
+       if else(c=='n')
+       {cout << "you not agreed";
+
+        }
+    }
+}
+    return 0;
+}
