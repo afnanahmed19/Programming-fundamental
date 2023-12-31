ccbfa920dd0c2f96ec09cf062f526ce7b5c66e71
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2023-12-31 12:02:19 +0000

    hola

--- DSA-Sir babar series/Lecture002 Write First C++ Program/firstProg.cpp
+++ DSA-Sir babar series/Lecture002 Write First C++ Program/firstProg.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main(){
 
- int slices;
- cout << "YO fatty how many pieces of pizza you eat?: ";
- cin >> slices;
+     int slices;
+     cout << "YO fatty how many pieces of pizza you eat?: ";
+      cin >> slices;
 
 
  cout << " You Have " << slices << " slices of pizza." <<endl;
