81a05fa6503aa0f90e9930fa058bdac4ed8df521
Afnan Ahmed <136823903+afnanahmed19@users.noreply.github.com>
2024-01-03 16:10:10 +0000

    update

---
+++ DSA-Sir babar series copy/Lecture002 Write First C++ Program/firstProg.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+     int slices;
+     cout << "YO fatty how many pieces of pizza you eat?: ";
+      cin >> slices;
+
+
+ cout << " You Have " << slices << " slices of pizza." <<endl;
+  
+  return 0; 
+}
